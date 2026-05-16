120dc249092281a43e7d01c46d5bdce49849552a
1 <1>
2026-05-16 16:11:38 +0300

    Экран товаров собран

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
 import { View, Text, FlatList } from "react-native";
 import products from '../product.json';
 import ProductListItem from "../components/ProductListItem";
-import { Button,ButtonText } from "@/components/ui/button";
+import { Button,ButtonGroup,ButtonText } from "@/components/ui/button";
 
 
 
 export default function HomeScreen(){
     return (    
          
-            // <FlatList
-            //     data={products}
-            //     renderItem={({ item }) => <ProductListItem product={item} />}
+            <FlatList
+                data={products}
+                numColumns={2}
+                contentContainerClassName="gap-2"
+                columnWrapperClassName="gap-2"
+                renderItem={({ item }) => <ProductListItem product={item} />}
                 
-            // />
-
-            <Button>
-                <ButtonText>Click me</ButtonText>
-            </Button>
+            />
+        
+           
          
     )
+
 }
